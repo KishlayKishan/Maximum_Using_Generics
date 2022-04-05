61c0eb2646a563edb3721754eb678ab436c0fb39
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-05 08:40:48 +0530

    Addede:Welcome to the Perogram to Find Maximum Problem Using Generics

---
+++ src/com/bridgelabz/maximumgeneric/Generic_main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.maximumgeneric;
+
+public class Generic_main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the program to find maximum problem using Generics");
+
+	}
+
+}
